d03f926bb478fa1d69ac603335587f1efc6ea63b
AYAITABASHI <119820092+AYA-ITABASHI@users.noreply.github.com>
2022-12-21 22:44:00 +0000

    [Add]編集と保存機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,35 @@
 class BooksController < ApplicationController
+
   def new
+    @books=Book.new
+  end
+
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def index
+    @book=Book.all
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+   book=Book.find(params[:id])  
+   book.update(book.params)
+   redirect_to book_path(book.id)
+  end  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
