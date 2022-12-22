d60cd0bcbe7e585defec8b8620f5a6cac2df79b6
AYAITABASHI <119820092+AYA-ITABASHI@users.noreply.github.com>
2022-12-22 22:08:47 +0000

    [Add]削除と編集機能の設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,12 +21,18 @@ class BooksController < ApplicationController
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def update
-   book=Book.find(params[:id])  
-   book.update(book.params)
+   book=Book.find(params[:id])
+   book.update(book_params)
    redirect_to book_path(book.id)
-  end  
+  end
+
+  def destroy
+   book=Book.find(params[:id])
+   book.destroy
+   redirect_to '/books'
+  end
 
   private
   def book_params
